54b65caf8e1a4a0f1a1dea2e32bd44632de06bdd
Nate Burdick <nate@thep4x.com>
2021-02-20 07:41:02 -0600

    Remove App.config

--- E-Radar/EmailGetter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace E_Radar
-{
-    public class EmailGetter
-    {
-        
-    }
-}
---
+++ E-Radar/MailGetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MailKit;
+using MailKit.Search;
+using MailKit.Net;
+using MailKit.Net.Imap;
+
+namespace E_Radar
+{
+    public class MailGetter
+    {
+        public async Task GetNewMessages()
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect();
+            }
+        }
+    }
+}
